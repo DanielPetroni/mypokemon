abbd97909c28297c653fcdec7e05d4996a0dc2b9
DanielPetroni <daniel.petroni25@gmail.com>
2021-02-11 13:13:19 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/petroni/mypokemon/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.petroni.mypokemon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:mypokemon/Screens/SplashScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyPokemon',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
