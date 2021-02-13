f9c6c67d7dc66caa409e0a4d31eb58435d30eb1d
DanielPetroni <daniel.petroni25@gmail.com>
2021-02-13 17:21:40 -0300

    Criado controller de navegação

--- lib/controller/NavigatorController.dart
+++ lib/controller/NavigatorController.dart
@@ -1,12 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mypokemon/Screens/HomePage/HomePage.dart';
-import 'package:mypokemon/Screens/Pokedex/PokedexScreen.dart';
 
 class NavigatorControler extends GetxController {
   final currentIndex = 0.obs;
-
-  List<Widget> pages = [HomePageScreen(), PokedexScreen()];
 
   void updatePage(int index) {
     currentIndex.value = index;
