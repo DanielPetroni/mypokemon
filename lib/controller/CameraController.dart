bc3574000cf4b329964a871a422c81342f273ef1
DanielPetroni <daniel.petroni25@gmail.com>
2021-02-13 21:10:14 -0300

    A imagem selecionada agora é salva na pasta do projeto
    Salvando a imagem selecionada para a pasta "ImagesPokemons" do projeto, para evitar bugs caso o usuário delete a imagem do diretório selecionado por ele.

--- lib/controller/CameraController.dart
+++ lib/controller/CameraController.dart
@@ -1,18 +1,21 @@
 import 'dart:io';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CameraController extends GetxController {
   Rx<File> image = File('').obs;
   final picker = ImagePicker();
   Future getImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final String path = directory.path;
     if (pickedFile != null) {
-       print(pickedFile.path);
-      image.value = File(pickedFile.path);
-      print(pickedFile.path);
+      File picikedCopy = await File(pickedFile.path).copy(path+'ImagesPokemons');
+      image.value = File(picikedCopy.path);
+      print(picikedCopy.path);
     } else {
-      print('No image selected.');
+      print('Nenhuma imagem selecionada.');
     }
   }
 }
